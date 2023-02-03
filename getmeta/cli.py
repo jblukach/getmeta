d6e9666bbf72fd4e8f9a2e2e480e2fa4a4686fb0
John Lukach <84751182+jblukach@users.noreply.github.com>
2023-02-03 01:58:53 +0000

    0.6.2

--- getmeta/cli.py
+++ getmeta/cli.py
@@ -107,6 +107,83 @@ async def parseonlypath(onlypath):
         onlypath = '\\'.join(out)
     return onlypath
 
+async def parser(p):
+    try:
+        uid = p.owner()
+    except:
+        uid = '-'
+        pass
+    try:
+        gid = p.group()
+    except:
+        gid = '-'
+        pass
+    try:
+        mask = oct(p.stat().st_mode)
+    except:
+        mask = '-'
+        pass
+    try:
+        mtime = str(p.stat().st_mtime)
+    except:
+        mtime = '-'
+        pass
+    if p.is_file() == True:
+        kind = 'FILE'
+        try:
+            size =  p.stat().st_size		
+        except: 
+            size = 0
+            pass
+        if size == 0:
+            md5_file = 'EMPTY'
+            sha256_file = 'EMPTY'
+            magic_file = 'EMPTY'
+        elif size > 104857599:
+            md5_file = 'LARGE'
+            sha256_file = 'LARGE'
+            magic_file = 'LARGE'
+        else:
+            hashes = await hasher(str(p))
+            out = hashes.split('|')
+            md5_file = out[0]
+            sha256_file = out[1]
+            magic_file = await mime(str(p))
+        fullpath = await normalizepath(str(p))
+        meta = await matchmeta(fullpath)
+        out = meta.split('|')
+        md5_path = out[0]
+        sha256_path = out[1]
+        directory = await parseonlypath(fullpath)
+        meta = await matchmeta(directory)
+        out = meta.split('|')
+        md5_dir = out[0]
+        sha256_dir = out[1]
+        filename = await parsefilename(fullpath)
+        meta = await matchmeta(filename)
+        out = meta.split('|')
+        md5_name = out[0]
+        sha256_name = out[1]
+    else:
+        kind = 'DIR'
+        size = '-'
+        md5_file = '-'
+        sha256_file = '-'
+        magic_file = '-'
+        md5_path = '-'
+        sha256_path = '-'
+        md5_name = '-'
+        sha256_name = '-'
+        directory = await normalizepath(str(p))
+        meta = await matchmeta(str(p))
+        out = meta.split('|')
+        md5_dir = out[0]
+        sha256_dir = out[1]
+    value = str(p)+'|'+kind+'|'+str(size)+'|'+md5_file+'|'+sha256_file+'|'+magic_file+'|'+ \
+            uid+'|'+gid+'|'+mask+'|'+mtime+'|'+md5_path+'|'+md5_dir+'|'+md5_name+'|'+ \
+            sha256_path+'|'+sha256_dir+'|'+sha256_name+'\n'
+    return value
+
 async def start():
     print('--------------------------------')
     print('GETMETA v'+__version__)
@@ -115,82 +192,15 @@ async def start():
         await f.write('path|source|size|md5|sha256|magic|uid|gid|mask|mtime|md5path|md5dir|md5name|sha256path|sha256dir|sha256name\n')
         root = PurePath(Path.cwd()).anchor
         path = Path(root)
-        for p in Path(path).rglob('*'):
-            try:
-                uid = p.owner()
-            except:
-                uid = '-'
-                pass
-            try:
-                gid = p.group()
-            except:
-                gid = '-'
-                pass
-            try:
-                mask = oct(p.stat().st_mode)
-            except:
-                mask = '-'
-                pass
-            try:
-                mtime = str(p.stat().st_mtime)
-            except:
-                mtime = '-'
-                pass
-            if p.is_file() == True:
-                kind = 'FILE'
-                try:
-                    size =  p.stat().st_size		
-                except: 
-                    size = 0
-                    pass
-                if size == 0:
-                    md5_file = 'EMPTY'
-                    sha256_file = 'EMPTY'
-                    magic_file = 'EMPTY'
-                elif size > 104857599:
-                    md5_file = 'LARGE'
-                    sha256_file = 'LARGE'
-                    magic_file = 'LARGE'
+        for p in Path(path).glob('*'):
+            if str(p) != '/proc':
+                if p.is_file() == True:
+                    value = await parser(p)
+                    await f.write(value)
                 else:
-                    hashes = await hasher(str(p))
-                    out = hashes.split('|')
-                    md5_file = out[0]
-                    sha256_file = out[1]
-                    magic_file = await mime(str(p))
-                fullpath = await normalizepath(str(p))
-                meta = await matchmeta(fullpath)
-                out = meta.split('|')
-                md5_path = out[0]
-                sha256_path = out[1]
-                directory = await parseonlypath(fullpath)
-                meta = await matchmeta(directory)
-                out = meta.split('|')
-                md5_dir = out[0]
-                sha256_dir = out[1]
-                filename = await parsefilename(fullpath)
-                meta = await matchmeta(filename)
-                out = meta.split('|')
-                md5_name = out[0]
-                sha256_name = out[1]
-            else:
-                kind = 'DIR'
-                size = '-'
-                md5_file = '-'
-                sha256_file = '-'
-                magic_file = '-'
-                md5_path = '-'
-                sha256_path = '-'
-                md5_name = '-'
-                sha256_name = '-'
-                directory = await normalizepath(str(p))
-                meta = await matchmeta(str(p))
-                out = meta.split('|')
-                md5_dir = out[0]
-                sha256_dir = out[1]
-
-            await f.write(str(p)+'|'+kind+'|'+str(size)+'|'+md5_file+'|'+sha256_file+'|'+magic_file+'|'+ \
-                          uid+'|'+gid+'|'+mask+'|'+mtime+'|'+md5_path+'|'+md5_dir+'|'+md5_name+'|'+ \
-                          sha256_path+'|'+sha256_dir+'|'+sha256_name+'\n')
+                    for s in Path(p).rglob('*'):
+                        value = await parser(s)
+                        await f.write(value)
 
 def main():
     asyncio.run(start())
